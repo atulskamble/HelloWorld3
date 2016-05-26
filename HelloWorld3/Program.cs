f05606716c19df4bb477b582bc91dad61a252671
Atul.Kamble <Atul.Kamble@03RNBL-NTPC0203>
2016-05-26 16:13:26 +0200

    modfied changes

--- HelloWorld3/Program.cs
+++ HelloWorld3/Program.cs
@@ -10,17 +10,7 @@ namespace HelloWorld3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World3");
-
-            Console.WriteLine("Github 3");
-
-            Console.WriteLine("Github 4");
-
-            Console.WriteLine("Change from Clone Repo");
-
-            Console.WriteLine("modified from HelloWorld3 repo");
-
-            Console.WriteLine("modified from CloneRepo");
+           Console.WriteLine("modified from CloneRepo");
         }
     }
 }
